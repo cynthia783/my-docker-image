e7a6743c31f2434e36a11f9764e07e2ff86ee9cf
cynthia <cynthia.ramaka@gmail.com>
2024-12-06 10:42:50 +0100

    docker image

---
+++ public/index.php
@@ -0,0 +1,15 @@
+# Utilise une image officielle de PHP avec Apache
+FROM php:8.2-apache
+
+# Copier le code dans le conteneur
+COPY public/ /var/www/html/
+
+# Exposer le port 80 pour le serveur web
+EXPOSE 80
+
+
+
+index.php
+
+<?php
+echo "Hello image docker";
